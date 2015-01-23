3e8129a73eef4b1cf2d2689ffa471c7811211c14
TomPickett <Pickett.tom7@gmail.com>
2015-01-22 19:18:10 -0500

    Created First Project - Lesson 1

---
+++ src/Player.java
@@ -0,0 +1,16 @@
+
+public class Player {
+
+	public static void main(String[] args) {
+		int strength = 10;
+		System.out.println("Hello World!" + strength);
+	
+		Person tom = new Person();
+		tom.walks();
+		
+		tom.walks();
+		
+		tom.walks();
+	}
+
+}
