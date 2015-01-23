bfccee4454986da55f5a00487cf7fd2f03180ec6
TomPickett <Pickett.tom7@gmail.com>
2015-01-22 19:51:36 -0500

    Updated Player class to work with modified person class

--- src/Player.java
+++ src/Player.java
@@ -5,10 +5,10 @@ public class Player {
 		int year = 2015;
 		System.out.println("Hello World! It's " + year + "!");
 	
-		Person tom = new Person("Tom", 225.3f);
+		Person tom = new Person("Tom", 225.3f, 6.0f);
 		tom.walks();
 		
-		Person mike = new Person("Mike", 180.9f);
+		Person mike = new Person("Mike", 180.9f, 6.5f);
 		mike.walks();
 		
 	}
